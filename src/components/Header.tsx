d445c2f7dcd477b068f56448335d94646c38deb8
Rainan Carneiro Araújo <rainanaraaujo@gmail.com>
2023-05-23 15:27:13 +0100

    responsive menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { EnvelopeSimple, InstagramLogo, Phone } from "phosphor-react";
+import MenuHamburger from "./MenuHamburger";
 
 interface HeaderProps {
   hideNav?: boolean;
@@ -11,10 +12,10 @@ export default function Header({ hideNav = false, currentPage }: HeaderProps) {
   return (
     <header className="w-full max-w-7xl px-20 m-auto flex flex-wrap justify-between items-center p-6 ">
       <Link href="/">
-        <Image width={120} height={120} src="/images/logoiphea.svg" alt="" />
+        <Image width={110} height={110} src="/images/logoiphea.svg" alt="" />
       </Link>
       {!hideNav ? (
-        <nav>
+        <nav className="hidden md:block">
           <ul className="flex justify-between gap-2 ">
             <Link href="/" className={`hover:font-bold duration-200`}>
               <li>Início</li>
@@ -54,14 +55,14 @@ export default function Header({ hideNav = false, currentPage }: HeaderProps) {
           </ul>
         </nav>
       ) : (
-        <div className="flex gap-3">
+        <div className="flex gap-4 md:gap-3">
           <Link
             className="flex items-center duration-100 hover:font-semibold"
             href={""}
             target="_blank"
           >
             <Phone size={20} weight="fill" className="text-blue-900" />
-            <span className="ml-2">11 99999 9999</span>
+            <span className="ml-2 hidden md:block">11 99999 9999</span>
           </Link>
           <Link
             className="flex items-center duration-100 hover:font-semibold"
@@ -69,7 +70,7 @@ export default function Header({ hideNav = false, currentPage }: HeaderProps) {
             target="_blank"
           >
             <EnvelopeSimple size={20} weight="fill" className="text-blue-900" />
-            <span className="ml-2">contato@iphea.org.br</span>
+            <span className="ml-2  hidden md:block">contato@iphea.org.br</span>
           </Link>
           <Link
             className="flex items-center duration-100 hover:font-semibold"
@@ -77,16 +78,18 @@ export default function Header({ hideNav = false, currentPage }: HeaderProps) {
             target="_blank"
           >
             <InstagramLogo size={20} weight="fill" className="text-blue-900 " />
-            <span className="ml-2">@institutoiphea</span>
+            <span className="ml-2  hidden md:block">@institutoiphea</span>
           </Link>
         </div>
       )}
       <button
         className="bg-blue-900 text-sm text-white py-3 
-        px-5 rounded-md hover:bg-blue-950 duration-200"
+        px-5 rounded-md hover:bg-blue-950 duration-200
+        hidden md:block"
       >
         Área do Colaborador
       </button>
+      <MenuHamburger />
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,8 +22,23 @@ module.exports = {
       animation: {
         fadeIn: "fadeIn 0.2s ease-in-out",
         fadeOut: "fadeOut 0.2s ease-in-out",
+        "scale-in": "scale-in 0.2s ease-in-out",
+        "slide-down": "slide-down 0.6s cubic-bezier(0.16, 1, 0.3, 1)",
+        "slide-up": "slide-up 0.6s cubic-bezier(0.16, 1, 0.3, 1)",
       },
       keyframes: (theme) => ({
+        "scale-in": {
+          "0%": { opacity: 0, transform: "scale(0)" },
+          "100%": { opacity: 1, transform: "scale(1)" },
+        },
+        "slide-down": {
+          "0%": { opacity: 0, transform: "translateY(-10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        "slide-up": {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
         fadeIn: {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
@@ -35,5 +50,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("tailwindcss-radix")()],
 };
